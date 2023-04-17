12548ce5705bfa5712d2b39ba9dfe0971e14d9a3
Rémi Connesson <remiconnesson2@gmail.com>
2023-04-17 11:51:21 +0200

    Set up @vueflow/core with minimal repro of bug

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,31 @@
 <script setup lang="ts">
 import { useDark, useToggle } from "@vueuse/core";
+import { VueFlow  } from '@vue-flow/core'
+import { ref } from 'vue'
+
 const isDark = useDark();
 const toggleDark = useToggle(isDark);
+
+
+const elements = ref([
+  // Nodes
+  // An input node, specified by using `type: 'input'`
+  { id: '1', type: 'input', label: 'Node 1', position: { x: 250, y: 5 } },
+
+  // Default nodes, you can omit `type: 'default'`
+  { id: '2', label: 'Node 2', position: { x: 100, y: 100 }, },
+  { id: '3', label: 'Node 3', position: { x: 400, y: 100 } },
+
+  // An output node, specified by using `type: 'output'`
+  { id: '4', type: 'output', label: 'Node 4', position: { x: 400, y: 200 } },
+
+  // Edges
+  // Most basic edge, only consists of an id, source-id and target-id
+  { id: 'e1-3', source: '1', target: '3' },
+
+  // An animated edge
+  { id: 'e1-2', source: '1', target: '2', animated: true },
+])
 </script>
 
 <template>
@@ -10,10 +34,17 @@ const toggleDark = useToggle(isDark);
     Dark: {{ isDark }} , click to change
   </button>
   <span class="block classic-apply">this is the target of an @apply rule</span>
+  <div class="w-[1400px] h-[1000px]" >
+    <VueFlow v-model="elements" />
+  </div>
 </template>
 
 <style scoped>
 .classic-apply {
   @apply text-xl text-orange-700 dark:text-white
+}
+
+:deep(.vue-flow__node){
+  @apply cursor-pointer bg-green-100 dark:text-red-100 dark:bg-red-700
 }
 </style>
